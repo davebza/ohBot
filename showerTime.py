4aab6d7c912b4388886965ba35a558cb7928a795
davebza <daveb.za@gmail.com>
2019-01-30 17:44:01 +0800

    Written up the first move and speech list for the shower choice program

--- showerTime.py
+++ showerTime.py
@@ -2,16 +2,56 @@ from ohbotMac import ohbot as ob
 from moveLib import randomMotionSmall, randomMotionBig, endAndRest, listOfMoves, listOfMotors, chooseBigOrSmall
 import random
 
+def chooseShower():
+
+    choice = random.randint(1, 2)
+    print(choice)
+
+    if choice == 1:
+        showerFirst = "Michelle"
+    elif choice == 2:
+        showerFirst = "Sean"
+
+    return showerFirst
+
 ob.reset()
-ob.wait(2)
+ob.wait(1)
 
 ob.setVoice("Samantha")
 
-listOfMoves = [i for i in range(random.randint(50, 100))]
+ob.say("Ah I'm awake. Did you need me?")
+
+listOfMoves = [i for i in range(random.randint(40, 60))]
 print(len(listOfMoves))
 
 for move in listOfMoves:
     chooseBigOrSmall()
+
+ob.reset()
+ob.wait(1)
+
+ob.say("Hi, Sean Hi Michelle")
+
+ob.say("Should I choose who will shower first tonight?")
+ob.wait(1)
+
+ob.say("Let me see")
+ob.wait(.3)
+
+showerFirst = chooseShower()
+
+ob.say("I've decided!")
+ob.say("The kid who is showering first is")
+
+ob.say(showerFirst)
+
+listOfMoves = [i for i in range(random.randint(40, 60))]
+print(len(listOfMoves))
+
+ob.reset()
+ob.wait(1)
+
+ob.say("Have a good shower" + showerFirst)
 
 ob.reset()
 ob.wait(1)
